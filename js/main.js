3385fd81c9dbfc76d822090c70b0ba67436fca52
alamin <alaminhosen44000@gmail.com>
2022-12-17 16:34:21 +0600

    fix-12/17/2022

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@
     navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
   });
 
+
+  // news-button
+  $('.news-expand-button').click(function(){
+    $(this).closest('.news-single').addClass('active');
+    let height = $(this).closest('.news-single').find('.news-s__body__inner').outerHeight();
+    $(this).closest('.news-single').find('.news-s__body').css('height', (height + 'px'));
+  });
+
         
 
 })(jQuery);
